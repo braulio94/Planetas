91942bea4e03d4653c3149506e36de0961e54343
Braulio Cassule <brauliocassule94@gmail.com>
2018-11-13 08:10:36 +0100

    add loading images with result

--- app/src/main/java/com/brauliocassule/androidvisionarios2018/MainActivity.java
+++ app/src/main/java/com/brauliocassule/androidvisionarios2018/MainActivity.java
@@ -1,19 +1,33 @@
 package com.brauliocassule.androidvisionarios2018;
 
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView planetRecyclerView;
     PlanetAdapter adapter;
+    ImageView addImageView;
+    Dialog addItemDialog;
+    String mCurrentPhotoPath;
+    int GALLERY_INTENT = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +42,66 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                adapter.planets.add(new Planet("Saturno", "Planeta muito, muito distante", R.drawable.saturn));
-                adapter.notifyDataSetChanged();
+                showDialog();
             }
         });
+    }
+
+    public void showDialog(){
+        addItemDialog = new Dialog(MainActivity.this);
+        addItemDialog.setContentView(R.layout.add_item_dialog);
+        addImageView = addItemDialog.findViewById(R.id.new_image);
+        addImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_INTENT);
+            }
+        });
+        addItemDialog.setTitle("Adicionar novo item");
+        addItemDialog.show();
+    }
+
+    private void getImage() {
+        int targetW = addImageView.getWidth();
+        int targetH = addImageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        //bmOptions.inPurgeable = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        //bitmap = ImageHelper.rotatedBitmap(bitmap, mCurrentPhotoPath);
+        addImageView.setImageBitmap(bitmap);
+    }
+
+    private String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(data != null && requestCode == RESULT_OK){
+            Uri uri = data.getData();
+            mCurrentPhotoPath = getRealPathFromURI(uri);
+            getImage();
+        }
     }
 }
 
